f2831af044732bf21e000c02b05f04a076c11267
eric.shady <darkercookies@gmail.com>
2023-02-14 16:47:28 +0800

    react essential: adding the TimeAgo component

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,13 +2,17 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
   const renderedPosts = posts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
-      <PostAuthor userId={post.user} />
+      <span>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </span>
       <p className="post-content">{post.content.substring(0, 100)}</p>
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
