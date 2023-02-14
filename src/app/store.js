e59fb9d63c3414e8719fa1d9e4f9d342bdbc4ba2
eric.shady <darkercookies@gmail.com>
2023-02-14 16:39:49 +0800

    react essential: adding author infor to the article and article list

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import postsReducer from '../features/posts/postSlices'
-
+import postsReducer from '../features/posts/postSlice'
+import usersReducer from '../features/users/userSlice'
 export default configureStore({
   reducer: {
     posts: postsReducer,
+    users: usersReducer,
   },
 })
--- src/features/posts/EditPostForm.js
+++ src/features/posts/EditPostForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 
-import { postUpdated } from './postSlices'
+import { postUpdated } from './postSlice'
 
 export const EditPostForm = ({ match }) => {
   const { postId } = match.params
