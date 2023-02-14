f3c0f147411e172fe4b622a9632cecab3f4057e1
eric.shady <darkercookies@gmail.com>
2023-02-14 16:51:38 +0800

    react essential: sorting the posts list

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -6,19 +6,27 @@ import { TimeAgo } from './TimeAgo'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
-  const renderedPosts = posts.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <span>
-        <PostAuthor userId={post.user} />
-        <TimeAgo timestamp={post.date} />
-      </span>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <Link to={`/posts/${post.id}`} className="button muted-button">
-        View Post
-      </Link>
-    </article>
-  ))
+
+  // Sort posts in reverse chronological order by datetime string
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date))
+
+  const renderedPosts = orderedPosts.map((post) => {
+    return (
+      <article className="post-excerpt" key={post.id}>
+        <h3>{post.title}</h3>
+        <span>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} />
+        </span>
+        <p className="post-content">{post.content.substring(0, 100)}</p>
+        <Link to={`/posts/${post.id}`} className="button muted-button">
+          View Post
+        </Link>
+      </article>
+    )
+  })
 
   return (
     <section className="posts-list">
