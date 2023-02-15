6efcdeb86a31af441ba11bc9f3ffbc4c0e3b8def
eric.shady <darkercookies@gmail.com>
2023-02-16 06:27:38 +0800

    react essential: adding notificationsList

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import postsReducer from '../features/posts/postSlice'
 import usersReducer from '../features/users/userSlice'
+import notificationsReducer from '../features/notifications/notificationsSlice'
+
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
+    notifications: notificationsReducer,
   },
 })
